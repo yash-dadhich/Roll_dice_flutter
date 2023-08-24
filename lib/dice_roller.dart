900b6128231b4675b5061a5efc56572d35488bca
Yash Vardhan Dadhich <51815545+yash-dadhich@users.noreply.github.com>
2023-08-24 16:03:09 +0530

    Initial commit

---
+++ lib/dice_roller.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var randomiser = Random();
+  var currentDiceRoll = 4;
+
+  void rollDice() {
+    setState(() {
+      currentDiceRoll = randomiser.nextInt(6) + 1; //0<>6
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/dice-$currentDiceRoll.png',
+          width: 200,
+        ),
+        const SizedBox(
+          height: 100,
+        ),
+        OutlinedButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 28),
+          ),
+          child: const Text("Roll Dice"),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/gradient_container.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:helloworld/dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+  // const GradientContainer({key}) : super(key: key);
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(child: DiceRoller()),
+    );
+  }
+}
+
+
+
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+//   // const GradientContainer({key}) : super(key: key);
+
+//   final List<Color> colors;
+
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(child: StyledText('Jai Shree Ram')),
+//     );
+//   }
+// }
